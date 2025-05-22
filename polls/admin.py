7e33b140e7ec9bb7a3794e859f9450f40f472987
Josep Ramon Aleixandri <jraleixandri@iesebre.com>
2025-05-22 13:00:51 +0200

    2n commit

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,7 @@ from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 1
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -16,4 +16,6 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "pub_date", "was_published_recently"]
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
+ 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, <b>world</b>. You're at the polls index.")
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
