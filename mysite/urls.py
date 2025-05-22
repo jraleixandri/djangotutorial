ea3372514371afe75358e195a8dfbf04040c5cc6
Josep Ramon Aleixandri <jraleixandri@iesebre.com>
2025-05-22 16:00:02 +0200

    3r commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from polls import views as pollviews
 
 urlpatterns = [
-    path( '', pollviews.index ),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path( '', pollviews.index ),
 ]
